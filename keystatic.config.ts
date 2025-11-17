f6d87300b65d94294ea066ff178308dd71d0ff48
prakosogalih <prakosogalih@proton.me>
2025-11-17 13:52:29 +0700

    change to nextjs

---
+++ keystatic.config.ts
@@ -0,0 +1,30 @@
+import { config, collection, fields } from '@keystatic/core';
+
+export const markdocConfig = fields.markdoc.createMarkdocConfig({});
+
+export default config({
+  storage: {
+    kind: 'local',
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'content/posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.markdoc({ label: 'Content' }),
+      },
+    }),
+    authors: collection({
+      label: 'Authors',
+      slugField: 'name',
+      path: 'content/authors/*',
+      schema: {
+        name: fields.slug({ name: { label: 'Name' } }),
+        bio: fields.markdoc({ label: 'Bio' }),
+      },
+    })
+  },
+});
