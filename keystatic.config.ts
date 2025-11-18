21fbd311d8c46a1d097eeb26e8d9e14832ce82f9
prakosogalih <prakosogalih@proton.me>
2025-11-18 21:42:45 +0700

    baru

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -12,12 +12,57 @@ export default config({
       format: { contentField: 'content' },
       schema: {
         title: fields.slug({ name: { label: 'Title' } }),
+        author: fields.relationship({
+          label: 'Author',
+          collection: 'authors',
+        }),
         content: fields.markdoc({
           label: 'Content',
           options: {
             image: {
               directory: 'src/assets/images/posts',
               publicPath: '../../assets/images/posts/',
+            },
+          },
+        }),
+      },
+    }),
+    authors: collection({
+      label: 'Authors',
+      slugField: 'name',
+      path: 'src/content/authors/*',
+      format: { contentField: 'bio' },
+      schema: {
+        name: fields.slug({ name: { label: 'Name' } }),
+        avatar: fields.image({
+          label: 'Avatar',
+          directory: 'src/assets/images/authors',
+          publicPath: '../../assets/images/authors/',
+        }),
+        bio: fields.markdoc({
+          label: 'Bio',
+          options: {
+            image: {
+              directory: 'src/assets/images/authors',
+              publicPath: '../../assets/images/authors/',
+            },
+          },
+        }),
+      },
+    }),
+    pages: collection({
+      label: 'Pages',
+      slugField: 'title',
+      path: 'src/content/pages/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.markdoc({
+          label: 'Content',
+          options: {
+            image: {
+              directory: 'src/assets/images/pages',
+              publicPath: '../../assets/images/pages/',
             },
           },
         }),
