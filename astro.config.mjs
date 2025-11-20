31a357630850decb73fcb3075b1c46c4e6783a4c
prakosogalih <prakosogalih@proton.me>
2025-11-20 17:54:38 +0700

    back to jekyll

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [react(), markdoc(), keystatic()],
-});
